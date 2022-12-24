aa10e40754a6eca4847c84d8ffc8713922825a34
lshaoqin <shaoqin.lu@gmail.com>
2022-12-24 13:52:47 +0800

    Fixed up associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :name, length: {maximum: 16, too_long: "%{count} characters is the maximum allowed."}
-    has_many :books, dependent: :destroy
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
 end
