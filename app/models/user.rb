89a9fc46b0a1ed171485323a826a962088e5ec53
lshaoqin <shaoqin.lu@gmail.com>
2022-12-29 14:48:45 +0800

    add password hashing to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     validates :name, presence: true
     validates :name, length: {maximum: 16, too_long: "%{count} characters is the maximum allowed."}
     has_many :posts, dependent: :destroy
