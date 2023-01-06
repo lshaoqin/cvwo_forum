99cb16b0b5a36a24eb601bada8992181dcba4a33
lshaoqin <shaoqin.lu@gmail.com>
2023-01-06 12:02:15 +0800

    add tags#new

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,30 @@
+class TagsController < ApplicationController
+    def new
+        name = params[:name]
+        decoded_id = JWT.decode(params[:token], 
+                                Rails.application.credentials.secret_key, 
+                                true)[0]['id']
+        post_id = params[:post_id]
+        post = Post.find(post_id)
+        #Determine weight of vote
+        weight = (post.user_id == decoded_id) ? 5 : 1
+        weight = (params[:positive]) ? weight : -weight
+        current_vote = Tag.find_by(name: name, 
+                                    user_id: decoded_id
+                                    post_id: post_id)
+        @tag = Tag.create(name: name,
+                            weight: weight
+                            post_id: post_id
+                            user_id: decoded_id)
+        if @tag.save
+            #delete/overwrite the old vote, if any
+            if current_vote
+                current_vote.destroy
+            end
+            render json: @tag, status: :created
+        else
+            render json: {errors: tag.errors}, status: :unprocessable_entity
+        end
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,19 @@ Rails.application.routes.draw do
   post 'comments/new', to: 'comments#new'
   post 'comments/edit', to: 'comments#edit'
   post 'comments/delete', to: 'comments#delete'
+
   get 'posts/index', to: 'posts#index'
   post 'posts/create', to: 'posts#create'
   post 'posts/edit', to: 'posts#edit'
   get 'posts/count', to: 'posts#count'
   get 'posts/get_by_id', to: 'posts#get_by_id'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
   get '/users', to: 'users#index'
   post '/users/login', to: 'users#login'
   post '/users/create', to: 'users#create'
+
+  post '/tags/new', to: 'tags#new'
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
