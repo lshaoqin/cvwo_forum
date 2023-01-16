f77aaaa73a929d766cd1b10f6cf3faaf784d68aa
lshaoqin <shaoqin.lu@gmail.com>
2023-01-16 15:32:02 +0800

    allow cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001', 'http://localhost:3000'
+      origins 'http://localhost:3001', 'http://localhost:3000', 'https://tagup.onrender.com'
       resource '*', headers: :any, methods: [:get, :post]
     end
   end
